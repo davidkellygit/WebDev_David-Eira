0a98577f6a1a9908bb37b1e5e747bb10254ac6f3
davidkellygit <c20324901@mytudublin.ie>
2022-11-27 09:26:51 +0000

    Colour added to web-page.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,44 +15,50 @@ function Header() {
     navigate('/')
   }
 
-  
-
   return (
     <>
-    <header className='header'>
-        <div className='logo'>
-            <Link to='/'>InstruRent</Link>
-        </div>
-        <ul>
-            {user ? (
-            <>  
-            <li>
-                <Link to='/upload'>
-                  <FaFileUpload /> Upload
+    {/*Makes Header Background Blue (Bootstrap)*/}
+    <div className='bg-info'>
+      <header className='header'>
+
+            <div className='logo'>
+                <Link to='/'>
+                  InstruRent
                 </Link>
-            </li>  
-            <li>
-                <button className='btn' onClick={onLogout}>
-                  <FaSignOutAlt /> Logout
-                </button>
-            </li>
-            </>
-            ) : (
-            <>
-            <li>
-                <Link to='/login'>
-                  <FaSignInAlt /> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                  <FaUser /> Register
-                </Link>
-            </li>
-            </>)}
-          
-        </ul>
-    </header>
+            </div>
+
+            <ul>
+                {user ? (
+                  <>
+                    <li>
+                        <Link to='/upload'>
+                          <FaFileUpload />Upload
+                        </Link>
+                    </li>  
+                    <li>
+                        <button className='btn' onClick={onLogout}>
+                          <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                  </ >
+                ) : (
+                  <>
+                    <li>
+                      <Link to='/login'>
+                        <FaSignInAlt /> Login
+                      </Link>
+                    </li>
+                    <li>
+                        <Link to='/register'>
+                          <FaUser /> Register
+                        </Link>
+                    </li>
+                  </>)}
+              
+            </ul>
+
+      </header>
+    </div>
     </>
   )
 }
